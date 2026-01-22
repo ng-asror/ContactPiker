7d954fade2ae62085f170932ca1f3193408b8ed6
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-22 18:09:23 +0500

    Update plan.ts

--- src/app/features/plan/services/plan.ts
+++ src/app/features/plan/services/plan.ts
@@ -1,24 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 import { IPlanReq, IPlansRes } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class Plan {
-  private http = inject(HttpClient);
+	private http = inject(HttpClient);
 
-  friends(): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}/plans/friends`);
-  }
+	friends(): Observable<any> {
+		return this.http.get<any>(`${environment.apiUrl}/plans/friends`);
+	}
 
-  createPlan(data: IPlanReq): Observable<void> {
-    return this.http.post<void>(`${environment.apiUrl}/plans/create/`, data);
-  }
+	createPlan(data: IPlanReq): Observable<void> {
+		return this.http.post<void>(`${environment.apiUrl}/plans/create/`, data);
+	}
 
-  plansList(): Observable<IPlansRes> {
-    return this.http.get<IPlansRes>(`${environment.apiUrl}/plans/list/`);
-  }
+	plansList(date?: string, filter_type?: 'new' | 'date'): Observable<IPlansRes> {
+		const params = new HttpParams();
+		if (date) {
+			params.append('date', date)
+		}
+		if (filter_type) {
+			params.append('filter_type', filter_type)
+		}
+		return this.http.get<IPlansRes>(`${environment.apiUrl}/plans/list/`, { params });
+	}
 }
