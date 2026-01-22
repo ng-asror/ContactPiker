982adf6bf26dcba1d0ee8e406d24b71b299b3a98
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-22 18:09:21 +0500

    Update plans.ts

--- src/app/features/plan/pages/plans/plans.ts
+++ src/app/features/plan/pages/plans/plans.ts
@@ -1,45 +1,81 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnDestroy,
-  OnInit,
-  resource,
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	OnDestroy,
+	OnInit,
+	resource,
+	signal,
 } from '@angular/core';
 import { Plan as PlanService } from '../../services';
 import { Telegram } from '../../../../core';
 import { firstValueFrom } from 'rxjs';
 
+interface DayPlans<T> {
+	day: number;
+	plans: T[];
+}
+
 @Component({
-  selector: 'app-plans',
-  imports: [],
-  templateUrl: './plans.html',
-  styleUrl: './plans.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-plans',
+	imports: [],
+	templateUrl: './plans.html',
+	styleUrl: './plans.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Plans implements OnInit, OnDestroy {
-  private telegram = inject(Telegram);
-  private planService = inject(PlanService);
+	private telegram = inject(Telegram);
+	private planService = inject(PlanService);
 
-  // Variables
-  protected currentMonth: string = new Date().toLocaleString('ru-RU', { month: 'long' });
-  protected currentYear: number = new Date().getFullYear();
-  protected currentMonthIndex: number = new Date().getMonth();
-  protected currentMonthDays: number = new Date(
-    this.currentYear,
-    this.currentMonthIndex,
-    0,
-  ).getDate();
+	// Variables
+	protected days = Array.from({ length: 31 }, (_, i) => i + 1);
+	protected currentMonth: string = new Date().toLocaleString('ru-RU', { month: 'long' });
+	protected currentYear: number = new Date().getFullYear();
+	protected currentMonthIndex: number = new Date().getMonth() + 1;
+	protected currentDay: number = new Date().getDate();
+	protected currentMonthDays: number = new Date(
+		this.currentYear,
+		this.currentMonthIndex,
+		0,
+	).getDate();
 
-  // resources
-  plans = resource({
-    loader: () => firstValueFrom(this.planService.plansList()),
-  });
+	// signals
+	selectDay = signal<number | null>(this.currentDay);
 
-  ngOnInit(): void {
-    this.telegram.showBackButton('/start');
-  }
-  ngOnDestroy(): void {
-    this.telegram.hiddeBackButton('/start');
-  }
+	// resources
+	plans = resource({
+		loader: () => firstValueFrom(this.planService.plansList()).then((res) => {
+			return this.buildMonthPlans(res.approved_and_yours_plans)
+		})
+	});
+
+	ngOnInit(): void {
+		this.telegram.showBackButton('/start');
+	}
+	ngOnDestroy(): void {
+		this.telegram.hiddeBackButton('/start');
+	}
+
+	// other functions
+	private buildMonthPlans<T extends { datetime: string }>(plans: T[]): DayPlans<T>[] {
+		const resoult: DayPlans<T>[] = Array.from(
+			{ length: this.currentMonthDays },
+			(_, i) => ({
+				day: i,
+				plans: []
+			})
+		)
+		for (const plan of plans) {
+			const planDate = new Date(plan.datetime);
+
+			if (planDate.getFullYear() === this.currentYear && planDate.getMonth() + 1 === this.currentMonthIndex) {
+				const dayIndex = planDate.getDate() - 1;
+				if (!resoult[dayIndex].plans) {
+					resoult[dayIndex].plans = []
+				}
+				resoult[dayIndex].plans!.push(plan)
+			}
+		}
+		return resoult
+	}
 }
