0da112d53c250fcc3a5531fc0ad74e7655474f8c
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-23 17:59:30 +0500

    add plan-detail page

---
+++ src/app/features/plan/pages/plan-detail/plan-detail.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-detail',
+  imports: [],
+  templateUrl: './plan-detail.html',
+  styleUrl: './plan-detail.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PlanDetail {
+
+}
