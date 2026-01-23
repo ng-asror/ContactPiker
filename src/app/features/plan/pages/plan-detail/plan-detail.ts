b410bd37fa91a203a3e4a7391fc38e35b0b7f79e
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-23 22:22:50 +0500

    Update plan-detail.ts

--- src/app/features/plan/pages/plan-detail/plan-detail.ts
+++ src/app/features/plan/pages/plan-detail/plan-detail.ts
@@ -1,15 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnDestroy, OnInit, resource, signal } from '@angular/core';
 import { Telegram } from '../../../../core';
+import { Plan as PlanService } from '../../services'
+import { firstValueFrom } from 'rxjs';
+import { DaysPipe } from '../../pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
 	selector: 'app-plan-detail',
-	imports: [],
+	imports: [DaysPipe, DatePipe],
 	templateUrl: './plan-detail.html',
 	styleUrl: './plan-detail.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlanDetail implements OnInit, OnDestroy {
 	private telegram = inject(Telegram);
+	private planService = inject(PlanService);
+
+	// inputs
+	id = input<string>('id');
+
+	// resources
+	plan = resource({
+		loader: () => firstValueFrom(this.planService.getPlan(this.id()))
+	})
+
+
 	ngOnInit(): void {
 		this.telegram.showBackButton('/plans')
 	}
