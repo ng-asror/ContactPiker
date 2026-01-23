65d8fcc69faaf062cde4f942437ae35e0f0f269a
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-23 22:22:46 +0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withInterceptors([authInterceptor])),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-  ],
+	providers: [
+		provideHttpClient(withInterceptors([authInterceptor])),
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes, withComponentInputBinding()),
+	],
 };
