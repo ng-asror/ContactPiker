c260ef7ef39f904833a77ae1ca0b8da5d02e2d2c
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-01-24 03:26:51 +0500

    Update account.ts

--- src/app/core/services/account.ts
+++ src/app/core/services/account.ts
@@ -1,13 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
+
+export interface IProfile {
+	id: number,
+	first_name: string,
+	last_name: string,
+	avatar: string,
+}
 
 @Injectable({
 	providedIn: 'root',
 })
 export class Account {
 	private http = inject(HttpClient);
+
+	// Subject & BehoviorSubject
+	profileSubject = new BehaviorSubject<IProfile | null>(null);
+	profile$ = this.profileSubject.asObservable();
 
 	login(data: {
 		initData: string;
@@ -20,5 +31,12 @@ export class Account {
 				invite_token: data.invite_token
 			},
 		);
+	}
+	profile(): Observable<IProfile> {
+		return this.http.get<IProfile>(`${environment.apiUrl}/accounts/profile/`).pipe(
+			tap((res: IProfile) => {
+				this.profileSubject.next(res)
+			})
+		)
 	}
 }
