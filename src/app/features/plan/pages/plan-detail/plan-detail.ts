f34a9d94fad952a434750c8735e46fc5b79c34ea
Асрор Шодиев <kworkasror@gmail.com>
2026-01-29 01:57:17 +0300

    Update plan-detail.ts

--- src/app/features/plan/pages/plan-detail/plan-detail.ts
+++ src/app/features/plan/pages/plan-detail/plan-detail.ts
@@ -1,60 +1,71 @@
-import { ChangeDetectionStrategy, Component, inject, input, OnDestroy, OnInit, resource } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  OnDestroy,
+  OnInit,
+  resource,
+} from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Telegram } from '../../../../core';
-import { Plan as PlanService } from '../../services'
+import { Plan as PlanService } from '../../services';
 import { firstValueFrom } from 'rxjs';
 import { DaysPipe } from '../../pipe';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 interface IPopup {
-	title: string;
-	message: string;
-	buttons: {
-		id: string;
-		type: 'default' | 'destructive' | 'cancel';
-		text: string;
-	}[];
+  title: string;
+  message: string;
+  buttons: {
+    id: string;
+    type: 'default' | 'destructive' | 'cancel';
+    text: string;
+  }[];
 }
 @Component({
-	selector: 'app-plan-detail',
-	imports: [DaysPipe, DatePipe],
-	templateUrl: './plan-detail.html',
-	styleUrl: './plan-detail.css',
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-plan-detail',
+  imports: [RouterLink, DaysPipe, DatePipe],
+  templateUrl: './plan-detail.html',
+  styleUrl: './plan-detail.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlanDetail implements OnInit, OnDestroy {
-	private telegram = inject(Telegram);
-	private clipboard = inject(Clipboard);
-	private planService = inject(PlanService);
+  private telegram = inject(Telegram);
+  private clipboard = inject(Clipboard);
+  private planService = inject(PlanService);
 
-	// inputs
-	id = input<string>('id');
+  // inputs
+  id = input<string>('id');
 
-	// resources
-	plan = resource({
-		loader: () => firstValueFrom(this.planService.getPlan(this.id()))
-	})
+  // resources
+  plan = resource({
+    loader: () => firstValueFrom(this.planService.getPlan(this.id())),
+  });
 
-	ngOnInit(): void {
-		this.telegram.showBackButton('/plans')
-	}
-	ngOnDestroy(): void {
-		this.telegram.hiddeBackButton('/plans')
-	}
+  ngOnInit(): void {
+    this.telegram.showBackButton('/plans');
+  }
+  ngOnDestroy(): void {
+    this.telegram.hiddeBackButton('/plans');
+  }
 
-	async sharePlan(): Promise<void> {
-		const popupData: IPopup = {
-			title: 'Предупреждение', message: 'Данная invite-ссылка действительна только для одного пользователя и используется один раз. Для каждого нового пользователя создавайте отдельную invite-ссылку.',
-			buttons: [
-				{ id: 'copy', type: 'default', text: 'Скопировать' },
-				{ id: 'share', type: 'default', text: 'Поделиться' },
-			]
-		}
-		await firstValueFrom(this.planService.sharePlan(this.id())).then((res) => {
-			const full_url = `https://t.me/share/url?url=${res.msg}`;
-			this.telegram.tg.showPopup(popupData, (buttonId: string) => {
-				if (buttonId === 'copy') this.clipboard.copy(res.link);
-				if (buttonId === 'share') this.telegram.open(full_url)
-			})
-		})
-	}
+  async sharePlan(): Promise<void> {
+    const popupData: IPopup = {
+      title: 'Предупреждение',
+      message:
+        'Данная invite-ссылка действительна только для одного пользователя и используется один раз. Для каждого нового пользователя создавайте отдельную invite-ссылку.',
+      buttons: [
+        { id: 'copy', type: 'default', text: 'Скопировать' },
+        { id: 'share', type: 'default', text: 'Поделиться' },
+      ],
+    };
+    await firstValueFrom(this.planService.sharePlan(this.id())).then((res) => {
+      const full_url = `https://t.me/share/url?url=${res.msg}`;
+      this.telegram.tg.showPopup(popupData, (buttonId: string) => {
+        if (buttonId === 'copy') this.clipboard.copy(res.link);
+        if (buttonId === 'share') this.telegram.open(full_url);
+      });
+    });
+  }
 }
