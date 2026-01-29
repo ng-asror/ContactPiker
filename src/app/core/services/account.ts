01f9707f6c3f6f67912ea7e1b4371ccd08f7c77d
Асрор Шодиев <kworkasror@gmail.com>
2026-01-29 18:52:41 +0300

    Update account.ts

--- src/app/core/services/account.ts
+++ src/app/core/services/account.ts
@@ -2,41 +2,53 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
+import { Telegram } from './telegram';
 
 export interface IProfile {
-	id: number,
-	first_name: string,
-	last_name: string,
-	avatar: string,
+  id: number;
+  first_name: string;
+  last_name: string;
+  avatar: string;
 }
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class Account {
-	private http = inject(HttpClient);
+  private http = inject(HttpClient);
+  private telegram = inject(Telegram);
 
-	// Subject & BehoviorSubject
-	profileSubject = new BehaviorSubject<IProfile | null>(null);
-	profile$ = this.profileSubject.asObservable();
+  // Subject & BehoviorSubject
+  profileSubject = new BehaviorSubject<IProfile | null>(null);
+  profile$ = this.profileSubject.asObservable();
 
-	login(data: {
-		initData: string;
-		invite_token: string | null;
-	}): Observable<{ access_token: string; refresh_token: string }> {
-		return this.http.post<{ access_token: string; refresh_token: string }>(
-			`${environment.apiUrl}/accounts/auth/telegram/`,
-			{
-				initData: data.initData,
-				invite_token: data.invite_token
-			},
-		);
-	}
-	profile(): Observable<IProfile> {
-		return this.http.get<IProfile>(`${environment.apiUrl}/accounts/profile/`).pipe(
-			tap((res: IProfile) => {
-				this.profileSubject.next(res)
-			})
-		)
-	}
+  login(data: {
+    initData: string;
+    invite_token?: string;
+  }): Observable<{ access_token: string; refresh_token: string }> {
+    const body: { initData: string; invite_token?: string } = { initData: data.initData };
+    if (data.invite_token) {
+      body.invite_token = data.invite_token;
+    }
+
+    return this.http
+      .post<{
+        access_token: string;
+        refresh_token: string;
+      }>(`${environment.apiUrl}/accounts/auth/telegram/`, JSON.stringify(body))
+      .pipe(
+        tap((res) => {
+          this.telegram.setCloudItem('access_token', res.access_token);
+          this.telegram.setCloudItem('refresh_token', res.refresh_token);
+        }),
+      );
+  }
+
+  profile(): Observable<IProfile> {
+    return this.http.get<IProfile>(`${environment.apiUrl}/accounts/profile/`).pipe(
+      tap((res: IProfile) => {
+        this.profileSubject.next(res);
+      }),
+    );
+  }
 }
