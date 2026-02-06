bdd1daa42083c5558e74f20478076d8997f02317
Асрор Шодиев <kworkasror@gmail.com>
2026-02-06 11:37:00 +0500

    Refactor ChatsList to use RouterLink and rooms resource

--- src/app/features/notification/pages/chats-list/chats-list.ts
+++ src/app/features/notification/pages/chats-list/chats-list.ts
@@ -1,27 +1,20 @@
 import { ChangeDetectionStrategy, Component, inject, resource } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Notification } from '../../services';
 import { firstValueFrom } from 'rxjs';
 import { DaysPipe } from '../../../plan';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chats-list',
-  imports: [DaysPipe],
+  imports: [RouterLink, DaysPipe],
   templateUrl: './chats-list.html',
   styleUrl: './chats-list.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatsList {
   private notifService = inject(Notification);
-  private router = inject(Router);
 
-  getNotifs = resource({
-    loader: () => firstValueFrom(this.notifService.notifs()).then((res) => res.notifications),
-  });
-
-  protected async readNotif(notif_id: string, room_id: string): Promise<void> {
-    await firstValueFrom(this.notifService.read(notif_id)).then(() => {
-      this.router.navigate(['chats', room_id]);
-    });
-  }
+  rooms = resource({
+    loader: () => firstValueFrom(this.notifService.rooms()).then(res => res.rooms)
+  })
 }
