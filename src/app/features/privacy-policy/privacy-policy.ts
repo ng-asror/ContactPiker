2ef877e3f3f73f6b8e324714c8e7791c84bc1b50
Асрор Шодиев <kworkasror@gmail.com>
2026-02-09 18:47:43 +0500

    Create privacy-policy.ts

---
+++ src/app/features/privacy-policy/privacy-policy.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  imports: [],
+  templateUrl: './privacy-policy.html',
+  styleUrl: './privacy-policy.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PrivacyPolicy {
+}
