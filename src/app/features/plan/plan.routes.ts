67d3e52db9100016d11f5aaee57acd8826db50e8
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2026-02-10 18:42:28 +0500

    Update plan.routes.ts

--- src/app/features/plan/plan.routes.ts
+++ src/app/features/plan/plan.routes.ts
@@ -1,26 +1,30 @@
 import { Routes } from '@angular/router';
 
 export const planRoutes: Routes = [
-  {
-    path: 'plans',
-    loadComponent: () => import('./pages').then((p) => p.Plans),
-    data: { menu: true },
-  },
-  {
-    path: 'plans/:id',
-    loadComponent: () => import('./pages').then((p) => p.PlanDetail),
-  },
-  {
-    path: 'create-plan',
-    loadComponent: () => import('./pages').then((p) => p.CreatePlan),
-  },
-  {
-    path: 'plans/:id/edit',
-    loadComponent: () => import('./pages').then((p) => p.UpdatePlan),
-  },
-  // For chat
-  {
-    path: 'chats/:id/plan',
-    loadComponent: () => import('./pages').then((p) => p.PlanDetail),
-  },
+	{
+		path: 'plans',
+		loadComponent: () => import('./pages').then((p) => p.Plans),
+		data: { menu: true },
+	},
+	{
+		path: 'plans/:id',
+		loadComponent: () => import('./pages').then((p) => p.PlanDetail),
+	},
+	{
+		path: 'create-plan',
+		loadComponent: () => import('./pages').then((p) => p.CreatePlan),
+	},
+	{
+		path: 'plans/:id/edit',
+		loadComponent: () => import('./pages').then((p) => p.UpdatePlan),
+	},
+	// For chat
+	{
+		path: 'chats/:id/plan',
+		loadComponent: () => import('./pages').then((p) => p.PlanDetail),
+	},
+	{
+		path: 'chats/:id/plan/edit',
+		loadComponent: () => import('./pages').then((p) => p.UpdatePlan),
+	},
 ];
